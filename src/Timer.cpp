11068720606fcc767d6c6a49728a36f52ca4bded
wem-wem <spidertargal@yahoo.co.jp>
2016-11-25 23:23:31 +0900

    残り０秒以下でtrueを返す【isTimeup()】と、タイマーリセット用の【resetElapsed()】追加

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -11,10 +11,19 @@ int Timer::getLimit() { return limit_; }
 void Timer::setFontSize(int fontSize) { fontSize_ = fontSize; }
 int Timer::getFontSize() { return fontSize_; }
 
+// 経過時間に制限時間の最大値を代入(毎試合最初に呼び出す想定)
+void Timer::resetElapsed() { ofResetElapsedTimeCounter(); }
+
 void  Timer::setup() {
   fontSetup();
   loadMaxTime();
   guiSetup();
+  ofAddListener(ofEvents().update, this, &Timer::update);
+}
+
+void Timer::update(ofEventArgs &args) {
+  // 最大時間 - 経過時間を表示用変数に代入
+  limit_ = maxTime_ - (ofGetElapsedTimef() - startTime_);
 }
 
 void Timer::draw() {
@@ -22,13 +31,24 @@ void Timer::draw() {
   gui_.draw();
 }
 
+bool Timer::isTimeup() {
+  if (limit_ > 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
 void Timer::guiSetup() {
+  reset_.addListener(this, &Timer::resetElapsed);
   save_.addListener(this, &Timer::saveFile);
   load_.addListener(this, &Timer::loadFile);
 
   gui_.setup();
   gui_.add(fontScale_.setup("FontScale", 100, 0, 1000));
   gui_.add(maxTime_.setup("MaxTime", 60, 0, 99));
+  gui_.add(reset_.setup("Time_Reset!"));
   gui_.add(save_.setup("Save_To_File"));
   gui_.add(load_.setup("Loda_From_File"));
   // xmlにセーブしてある値を最初に呼び出しておく
@@ -45,7 +65,7 @@ void Timer::fontSetup() {
 void Timer::loadMaxTime() {
   if (xml_.loadFile("Game/TimerSettings.xml")) {
     setMaxTime(xml_.getValue("group:MaxTime", 0));
-    resetLimit(); // 最大値のロードと同時に制限時間に同じ値を代入
+    setLimit(maxTime_); // 最大値のロードと同時に制限時間に同じ値を代入
   }
 }
 
@@ -56,24 +76,23 @@ void Timer::loadFontSize() {
   }
 }
 
-// 経過時間に制限時間の最大値を代入(毎試合最初に呼び出す想定)
-void Timer::resetLimit() {
-  setLimit(maxTime_);
-}
-
 // 画面上部、センターに表示
 void Timer::drawTimer() {
   ofPushMatrix();
   ofPushStyle();
+
   ofSetColor(255, 255, 0);
-  string text = ofToString((int)(limit_ - (ofGetElapsedTimef() - startTime_)));
+  string text;
+  // 表示するtextの中身を残り時間で切り替える
+  if (!isTimeup()) { text = ofToString((int)limit_);  }
+  else { text = ofToString(0); }
   float fontWidth = font_.stringWidth(text);
   float fontHeight = font_.stringHeight(text);
 
   ofTranslate(ofGetWidth() / 2, 0);
   ofScale((ofGetWidth() / fontScale_), (ofGetHeight() / fontScale_), 1);
+  font_.drawString(text, -fontWidth / 2, fontHeight); // タイマーの描画
 
-  font_.drawString(text, -fontWidth / 2, fontHeight);
   ofPopStyle();
   ofPopMatrix();
 }
--- src/Timer.h
+++ src/Timer.h
@@ -9,7 +9,6 @@
 class Timer {
 private:
   int limit_;  // 表示される残り時間
-  //int maxTime_;
   float startTime_; // 計測開始時の時間を保存
   float fontSize_;
   ofTrueTypeFont font_; // 表示する際のフォントとサイズを入れる箱
@@ -19,6 +18,7 @@ private:
   ofxPanel gui_;
   ofxFloatSlider fontScale_;
   ofxIntSlider maxTime_;
+  ofxButton reset_;
   ofxButton save_;
   ofxButton load_;
 
@@ -26,7 +26,6 @@ private:
   void fontSetup();
   void loadMaxTime();
   void loadFontSize();
-  void resetLimit();
   void drawTimer();
   void saveFile();
   void loadFile();
@@ -40,6 +39,9 @@ public:
   void setFontSize(int fontSize);
   int getFontSize();
 
+  void resetElapsed();  // タイマーリセット
   void setup();
+  void update(ofEventArgs &args);
   void draw();
+  bool isTimeup();
 };
